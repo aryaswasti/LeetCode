393310d7b11a38417acdf42bc295235e3cf750cf
Swasti <swastiarya4@gmail.com>
2022-09-26 10:13:52 +0530

    Time: 146 ms (11.19%), Space: 19.2 MB (57.57%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    void swap(int &num1, int &num2) {
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
-    }
-    
     void moveZeroes(vector<int>& nums) {
+        int end = nums.size()-1;
+        
+        while(end > 0) {
+            if(nums[end] != 0)
+                break;
+            end--;
+        }
+        
         int i = 0;
         
-        for(int j=0; j<nums.size(); j++) {
-            if(nums[j] != 0) {
-                swap(nums[i], nums[j]);
-                i++;
-            }
+        while(i<nums.size() && i!=end) {
+            if(nums[i] == 0) {
+                nums.erase(nums.begin() + i);
+                nums.push_back(0);
+                i--;
+                end--;
+            } 
+            i++;
         }
     }
 };
