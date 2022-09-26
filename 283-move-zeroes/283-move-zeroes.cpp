d583a32df15b5bae6d795280444a1448dbb56a52
Swasti <swastiarya4@gmail.com>
2022-09-26 10:16:13 +0530

    Time: 63 ms (18.97%), Space: 19.2 MB (83.24%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,24 +1,65 @@
 class Solution {
 public:
+    void swap(int &num1, int &num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    
     void moveZeroes(vector<int>& nums) {
-        int end = nums.size()-1;
-        
-        while(end > 0) {
-            if(nums[end] != 0)
-                break;
-            end--;
-        }
-        
         int i = 0;
         
-        while(i<nums.size() && i!=end) {
-            if(nums[i] == 0) {
-                nums.erase(nums.begin() + i);
-                nums.push_back(0);
-                i--;
-                end--;
-            } 
-            i++;
+        for(int j=0; j<nums.size(); j++) {
+            if(nums[j] != 0) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
         }
     }
 };
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int end = nums.size()-1;
+        
+//         while(end > 0) {
+//             if(nums[end] != 0)
+//                 break;
+//             end--;
+//         }
+        
+//         int i = 0;
+        
+//         while(i<nums.size() && i!=end) {
+//             if(nums[i] == 0) {
+//                 nums.erase(nums.begin() + i);
+//                 nums.push_back(0);
+//                 i--;
+//                 end--;
+//             } 
+//             i++;
+//         }
+//     }
+// };
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         vector<int> ans;
+//         int zeros = 0;
+        
+//         for(int i=0; i<nums.size(); i++) {
+//             if(nums[i] != 0)
+//                 ans.push_back(nums[i]);
+//             else if(nums[i] == 0)
+//                 zeros++;        
+//         }
+        
+//         while(zeros>0) {
+//             ans.push_back(0);
+//             zeros--;
+//         }
+//         nums = ans;
+//     }
+// };
