4ca9eeb2ab5b8fb5a78a72dd883a6b7f5499ea4e
Swasti <swastiarya4@gmail.com>
2024-01-26 19:37:09 +0530

    Time: 25 ms (41.12%), Space: 27.3 MB (9.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
+    //if k>n then k=k%n (after evern n rotation nums become intial arrary.)
+    //S.C. = O(1)
+    //Example: [1,2,3,4,5,6,7] k=3;
+    //First reverse index 0-4,after reverse nums : [4,3,2,1,5,6,7],
+    //Then reverse index 4-6, after reverse nums :[4,3,2,1,7,6,5],
+    //Finally reverse 0-n-1, after reverse nums:[5,6,7,1,2,3,4].
+    
     void rotate(vector<int>& nums, int k) {
-        vector<int> ans;
-        ans = nums;
-        int n = nums.size();
-        
-        if(k>n){
-            k = k%n; 
-        }
-        
-        int i = 0;
-        int j = n-k;
-        while(j<n){
-            nums[i] = ans[j];
-            i++;
-            j++;
-        }
-        
-        int l = 0;
-        while(i<n){
-            nums[i] = ans[l];
-            i++;
-            l++;
-        }
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin()+(n-k),nums.end());
+        reverse(nums.begin(),nums.begin()+(n-k));
+        reverse(nums.begin(),nums.end());
     }
 };
