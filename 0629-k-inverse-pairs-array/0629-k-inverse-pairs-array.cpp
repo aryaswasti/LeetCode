6b47ff14723723d7c2b6bfa856afa0c1dedfedf2
Swasti <swastiarya4@gmail.com>
2024-01-27 23:30:20 +0530

    Time: 1175 ms (19.78%), Space: 15.9 MB (31.87%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+private:
+    int MOD = 1e9 + 7;
+    int dp[1001][1001] = {};
+public:
+    int kInversePairs(int n, int k) {
+    int dp[1001][1001] = {1};
+    for (int N = 1; N <= n; ++N)
+        for (int K = 0; K <= k; ++K)
+            for (int i = 0; i <= min(K, N - 1); ++i)
+                dp[N][K] = (dp[N][K] + dp[N-1][K-i]) % MOD;
+    return dp[n][k];
+    }
+};
