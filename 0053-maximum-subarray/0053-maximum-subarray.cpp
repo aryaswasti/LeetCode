f0510278737baa5c2408d024757f0934fc5d1a1b
Swasti <swastiarya4@gmail.com>
2024-01-28 20:18:03 +0530

    Time: 79 ms (78.63%), Space: 70.3 MB (9.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        //keeps track of maximum sum
         int maxSum = nums[0];
+        //keeps track of current maximum sum
         int currentSum = nums[0];
 
         for (int i = 1; i < nums.size(); i++) {
+            //check currentElement > currentSum + currentElement
+            //as there are negatuve numbers current number maybe be more than the current sum
             currentSum = max(nums[i], currentSum + nums[i]);
+            //if currentSum is larger than te maxSum, replace it
             maxSum = max(maxSum, currentSum);
         }
 
