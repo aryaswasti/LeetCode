ae7d35a77f665aee60aab6b1a85abc70ae6627f0
Swasti <swastiarya4@gmail.com>
2024-01-28 19:14:14 +0530

    Time: 16 ms (43.73%), Space: 22 MB (9.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        //O(nlogn) - Using sorting
-        sort(nums.begin(), nums.end());
-        //return middle element
-        return nums[nums.size()/2];
+    int majorityElement(vector<int>& nums) {    
+        //Moore Voting Algorithm - O(n)
+        //if finding element occuring more than n/2 => count of candidate will always be more
+        int count = 0;
+        int candidate = nums[0];
         
-        
-        //O(n) - Moore Voting Algorithm
-//         int count = 0;
-//         int candidate = nums[0];
-        
-//         for(int i=0; i<nums.size(); i++){
-//             int current = nums[i];
+        for(int i=0; i<nums.size(); i++){
+            int current = nums[i];
             
-//             if(count == 0 && current != candiate){
-//                 candidate = current;
-//                 count++;
-//             } else if(current == candidate) {
-//                 count++;
-//             } else {
-//                 count--;
-//             }
-//         }
-//         return candidate;
+            //count=0 and current != candidate => assign current as new candidate
+            if(count == 0 && current != candidate){
+                candidate = current;
+                count++;
+            //current = candidate => increase count
+            } else if(current == candidate) {
+                count++;
+            //count > 0 and current != candidate => decrease count for candidate 
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+        
+        // Using sorting - O(nlogn) 
+        // sort(nums.begin(), nums.end());
+        // //return middle element
+        // return nums[nums.size()/2];
     }
 };
