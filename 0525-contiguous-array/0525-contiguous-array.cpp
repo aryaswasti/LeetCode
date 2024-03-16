6ff4f4dc21c69513426e32e76f9c1fc8585f0588
Swasti <swastiarya4@gmail.com>
2024-03-16 22:13:01 +0530

    Time: 82 ms (72.24%), Space: 87.9 MB (17.20%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,40 +1,37 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        //store difference of <countOne-CountZero, index>
-        unordered_map<int, int> umap;
-        //store count of 0
-        int zeros = 0;
-        //store count of 1
-        int ones = 0;
         //store maximum length od subArray
-        int maxLen = 0;
+        int maxLength = 0;
+        //sum till current element
+        int sum = 0;
+        // Maps sum to its first occurrence index
+        unordered_map<int, int> sumIndex; 
         
-        umap[0] = -1;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            //count 0
-            if(nums[i] == 0){
-                zeros++;
-            //count 1
-            } else if(nums[i] == 1){
-                ones++;
-            }
-            
-            //calculate difference between count of 0 and 1
-            int diff = zeros - ones;
-            
-            //check if same diff exist in prev index
-            if (umap.count(diff)) {
-                //if yes => sum from prevIndex to currIndex(of same difference) will be same
-                //i - umap[diff] => currentIndex - prevIndex
-                maxLen = max(maxLen, i - umap[diff]);
+        // Initialize the map with the sum 0 and its index -1
+        //this is used when sum encountered is 0
+        //and 0 sum means all elements from start of array can be included(-1)
+        sumIndex[0] = -1; 
+
+        for (int i = 0; i < nums.size(); ++i) {
+            // Convert 0 to -1 and 1 to 1
+            sum += (nums[i] == 0) ? -1 : 1;
+
+            //if sum already exists => prev occurance till current element is valid subArray
+            if (sumIndex.find(sum) != sumIndex.end()) {
+                maxLength = max(maxLength, i - sumIndex[sum]);
+            //if sum is encountered for first time => add it to Map
             } else {
-                //store first unique difference
-                umap[diff] = i;
+                sumIndex[sum] = i;
             }
         }
-        //return maxLength
-        return maxLen;
+        
+        //return maximum length of valid subArray
+        return maxLength;
     }
 };
