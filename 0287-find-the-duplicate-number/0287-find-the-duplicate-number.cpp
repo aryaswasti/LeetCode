c05decc62d265383d6cb629d1e8cab9f9c54a354
Swasti <swastiarya4@gmail.com>
2024-03-24 11:39:27 +0530

    Time: 79 ms (63.15%), Space: 63.5 MB (77.16%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,23 +1,51 @@
+// Floyd's Fast & Slow find the cycle entrance
+// Assume each element as pointer to array position.
+// Duplicate number will point to same array position 
+
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        
-        //iteratere through nums array
-        for(int i=0; i<nums.size(); i++){
-            //get posititive value of nums
-            //abs because in case of duplicate element the nums[i] value will be -ve => -ve index will give error
-            int index = abs(nums[i]);
-            
-            //check if the nums value at index is negative
-            if(nums[index] < 0){
-                //if it is nagative it means it was previously visited by the duplicate element
-                return index;
-            }
-            
-            //set the visited element at index as -ve
-            nums[index] = -nums[index];
+    static int findDuplicate(vector<int>& nums) {
+        // slow and fast pointer will intersect indicating a cycle
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(fast!=slow);
+
+        // Now move slow2 from start and slow from intersection point
+        // Now where they will intersect will be the start of cycle => answer
+        int slow2=nums[0];
+        while(slow!=slow2){
+            slow=nums[slow];
+            slow2=nums[slow2];
         }
-        
-        return 0;
+        return slow;
     }
 };
+
+
+// // Solution by making the vector negative
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+        
+//         //iteratere through nums array
+//         for(int i=0; i<nums.size(); i++){
+//             //get posititive value of nums
+//             //abs because in case of duplicate element the nums[i] value will be -ve => -ve index will give error
+//             int index = abs(nums[i]);
+            
+//             //check if the nums value at index is negative
+//             if(nums[index] < 0){
+//                 //if it is nagative it means it was previously visited by the duplicate element
+//                 return index;
+//             }
+            
+//             //set the visited element at index as -ve
+//             nums[index] = -nums[index];
+//         }
+        
+//         return 0;
+//     }
+// };
