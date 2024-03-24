95142975315f34db96f193ea68deb48fd0230497
Swasti <swastiarya4@gmail.com>
2024-03-24 11:22:05 +0530

    Time: 63 ms (96.10%), Space: 63.5 MB (93.58%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
+        //iteratere through nums array
         for(int i=0; i<nums.size(); i++){
+            //get posititive value of nums
+            //abs because in case of duplicate element the nums[i] value will be -ve => -ve index will give error
             int index = abs(nums[i]);
+            
+            //check if the nums value at index is negative
             if(nums[index] < 0){
+                //if it is nagative it means it was previously visited by the duplicate element
                 return index;
             }
+            
+            //set the visited element at index as -ve
             nums[index] = -nums[index];
         }
         
