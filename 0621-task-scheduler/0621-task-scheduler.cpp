15efaf0a22e61005114c1c7f099bdad492dbe6ac
Swasti <swastiarya4@gmail.com>
2024-03-26 23:46:13 +0530

    Time: 52 ms (52.42%), Space: 37.8 MB (86.93%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,36 +1,39 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int> mpp;
-        for(auto task : tasks){
-            mpp[task]++;
+        // Array to store the frequency of each task
+        int freq[26] = {0};
+        
+        // Count the frequency of each task
+        for(char task : tasks){
+            freq[task - 'A']++;
         }
         
-        priority_queue<int> pq;
-        for(auto i : mpp){
-            pq.push(i.second);
-        }
+        // Sort the frequencies in descending order
+        sort(begin(freq), end(freq));
         
-        queue<pair<int,int>> q;
-        int time = 0;
-        while(pq.size() > 0 or q.size() > 0){
-            if(pq.size() > 0){
-                int count = pq.top();
-                pq.pop();
-                time += 1;
-                --count;
-                if(count != 0) {
-                    q.push({count,time+n});   
-                }
-            }
-            if(q.size() > 0 and q.front().second <= time){
-                auto temp = q.front();q.pop();
-                pq.push(temp.first);
-            }
-            else if(pq.size()==0){
-                time++; // We need to do time -1 in the end because it will be increamenting it even when all task done
-            }
+        // Calculate the number of intervals needed by considering the task with the maximum frequency
+        int chunk = freq[25] - 1; // Maximum frequency - 1
+        int idel = chunk * n; // Initial estimate of idle slots
+        
+        // Iterate from the second highest frequency to the lowest frequency
+        for(int i = 24; i >= 0; i--){
+            // Update the idle slots by subtracting the minimum of chunk and frequency of the current task
+            // We use the minimum because we only need to insert idel when the remaining tasks are fewer than the chunk size
+            // if we had 4 As and 2 Bs, so 3 chunks, here we can easily fill 2 bs in those 3 chunks while maintiang the rules
+            // But in case Bs were more than the chunks, then the leftover B would be placed at the end
+            idel -= min(chunk, freq[i]);
         }
-        return time-1; 
+
+        // We return different answers based on different cases:
+        // If idel is less than 0, it means that all tasks can be scheduled without remaining idle slots
+        // In this case, we return the total number of tasks
+        // Otherwise, we return the total number of tasks plus the remaining idle slots
+        return idel < 0 ? tasks.size() : tasks.size() + idel;
     }
 };
