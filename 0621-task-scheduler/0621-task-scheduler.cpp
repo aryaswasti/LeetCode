7961293a5a7e527214ba2efc0796087f8efc41b9
Swasti <swastiarya4@gmail.com>
2024-03-26 23:16:02 +0530

    Time: 61 ms (36.88%), Space: 38 MB (59.96%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int freq[26] = {0};
-        for(char task : tasks){
-            freq[task - 'A']++;
+        unordered_map<char, int> taskCount;
+        for (char task : tasks) {
+            taskCount[task]++;
         }
-        sort(begin(freq) , end(freq));
-        int chunk = freq[25] - 1;
-        int idel = chunk * n;
-
-        for(int i=24; i>=0; i--){
-            idel -= min(chunk,freq[i]);
+        
+        int maxTask = 0;
+        for (auto& kv : taskCount) {
+            maxTask = max(maxTask, kv.second);
         }
-
-        return idel < 0 ? tasks.size() : tasks.size() + idel;
+        
+        int maxRepeats = 0;
+        for (auto& kv : taskCount) {
+            if (kv.second == maxTask) {
+                maxRepeats++;
+            }
+        }
+        
+        return max((int)tasks.size(), maxTask * (1 + n) - n + maxRepeats - 1);
     }
 };
