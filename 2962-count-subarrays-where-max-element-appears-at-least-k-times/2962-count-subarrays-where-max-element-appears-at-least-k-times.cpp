1d73c14748095682aea0a3028eb184afc039f99c
Swasti <swastiarya4@gmail.com>
2024-03-30 13:18:32 +0530

    Time: 130 ms (64.65%), Space: 120 MB (61.72%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        long long int max_num = *max_element(nums.begin(),nums.end()),count = 0;
-        long long int left = 0,right = 0,ans = 0;
-        while(right<nums.size()){
-            if(nums[right] == max_num)count++;
+        //get maximum element of array
+        long long int maxNum = *max_element(nums.begin(),nums.end());
+        
+        //size of nums
+        long long int n = nums.size();
+        //count of maximum element
+        long long int count = 0;
+        //number of subarray with at least k times maxNum => at least means (count >= k)
+        long long int ans = 0;
+        
+        //start sliding window
+        for(long long int right=0, left=0; right<n; right++){
+            //increase 'count' if right element = maxNum
+            if(nums[right] == maxNum){
+                count++;
+            }
             
-            while(count>=k){
-                if(nums[left]==max_num)count--;
+            //iterate till window is still valid subarray(i.e. atleast k maxNum)
+            while(count >= k){
+                //if left element to be removed from window = maxNum
+                if(nums[left] == maxNum){
+                    //decrease count of maxNum
+                    count--;
+                }
+                //n-right => gives remaining element of array beyond subarray
+                //we can add each element once to current window to make another valid subarray
+                //so ans = ans + all possible subarray of cuurent window(n-right)
+                ans+=(n-right);
+                //move left forward to reduce window size
                 left++;
-                ans += nums.size()-right;
-            }    
-            right++;
+            }
         }
 
-
         return ans;
-        
     }
 };
