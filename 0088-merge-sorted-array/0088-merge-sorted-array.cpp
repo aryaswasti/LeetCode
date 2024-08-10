664c3a1742479effea12f4e0a72959c2f707cd09
Swasti <swastiarya4@gmail.com>
2024-08-10 19:26:04 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (35.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,10 +5,13 @@ public:
         int j = n - 1; // Pointer for nums2
         int k = m + n - 1; // Pointer for the end of merged array
 
+        // it will not overwrite nums1 elements
         while (i >= 0 && j >= 0) {
+            // add nums[i] at k
             if (nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
                 i--;
+            // add nums2[j] at k
             } else {
                 nums1[k] = nums2[j];
                 j--;
@@ -17,6 +20,7 @@ public:
         }
 
         // Copy remaining elements from nums2, if any
+        // don't need to copy nums1 smaller elements as it is already in place(within nums1)
         while (j >= 0) {
             nums1[k] = nums2[j];
             j--;
